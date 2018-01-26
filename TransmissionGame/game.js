55bd4a2c7448c7802f4e195ba99e2dcc8173ec42
BATCH Anon <billy.rebecchi@googlemail.com>
2018-01-26 18:52:50 +0000

    Create function to draw a grid

--- TransmissionGame/game.js
+++ TransmissionGame/game.js
@@ -1,10 +1,49 @@
+// Store the canvas size
+var canvasSize = 600;
+
+// Store the number of grid squares
+var gridSquares = 20;
+
+// Store the individual grid dimensions
+var gridSquareSize;
+
 // Setup function run before game starts
 function setup() {
 		
 	// Create a canvas
-	createCanvas(600, 600);
+	createCanvas(canvasSize, canvasSize);
+	
+	// Set the grid size
+	gridSquareSize = canvasSize / gridSquares;
 		
-	// Set bgColor
-	background(255, 0, 200);
+}
+
+// Render function
+function draw() {
+	
+	// Call the function to draw the grid
+	drawGrid();
+	
+}
+
+// Function to draw a grid
+function drawGrid() {
+	
+	// For each row
+	for (var i = 0; i < gridSquares; i++) {
 		
+		// For each column
+		for (var j = 0; j < gridSquares; j++) {
+			
+			// Calculate the x and y
+			var x = i * gridSquareSize;
+			var y = j * gridSquareSize;
+			
+			// Draw the quad
+			quad(x,y, x+gridSquareSize,y, x+gridSquareSize,y+gridSquareSize, x,y+gridSquareSize);
+			
+		}
+		
+	}
+	
 }
