4a57f79c46088a2aa5c95d9ff3485bcc268e523a
BATCH Anon <billy.rebecchi@googlemail.com>
2018-01-26 19:23:18 +0000

    Add ability to select a square and colour it in

--- TransmissionGame/game.js
+++ TransmissionGame/game.js
@@ -7,6 +7,9 @@ var gridSquares = 20;
 // Store the individual grid dimensions
 var gridSquareSize;
 
+// List to hold the point
+var points = [];
+
 // Setup function run before game starts
 function setup() {
 		
@@ -24,6 +27,9 @@ function draw() {
 	// Call the function to draw the grid
 	drawGrid();
 	
+	// Call the function to draw the points
+	drawPoints();
+	
 }
 
 // Function to draw a grid
@@ -39,11 +45,71 @@ function drawGrid() {
 			var x = i * gridSquareSize;
 			var y = j * gridSquareSize;
 			
-			// Draw the quad
-			quad(x,y, x+gridSquareSize,y, x+gridSquareSize,y+gridSquareSize, x,y+gridSquareSize);
-			
+			// Draw the grid square
+			drawGridSquare(x, y, color(255));
 		}
 		
 	}
+	
+}
+
+// Function to draw a grid square
+function drawGridSquare(x, y, colour) {
+	
+	// Set the colour
+	fill(colour);
+			
+	// Draw the quad
+	quad(x,y, x+gridSquareSize,y, x+gridSquareSize,y+gridSquareSize, x,y+gridSquareSize);
+	
+}
+
+// Function to draw the points
+function drawPoints() {
+	
+	// For each point
+	for(var i = 0; i < points.length; i++) {
+		
+		// Get the x and y
+		x = points[i].x * gridSquareSize;
+		y = points[i].y * gridSquareSize;
+		
+		// Draw the gridsquare
+		drawGridSquare(x, y, color(255, 0, 200));
+		
+	}
+	
+}
+
+// Function to deal with mouse clicks
+function mouseClicked() {
+	
+	// Check that the mouse click is in the grid
+	if (!((mouseX >= 0 && mouseX <= canvasSize) && (mouseY >= 0 && mouseY <= canvasSize))) {
+		
+		// Return
+		return;
+		
+	}
+	
+	// Check the mouse button
+	if (mouseButton === LEFT) {
+		
+		// Get the x and y
+		var x = Math.floor(mouseX / gridSquareSize);
+		var y = Math.floor(mouseY / gridSquareSize);
+		
+		// Add a new point
+		points.push(Point(x,y));
+		
+	}
+	
+}
+
+// Function to return a point
+function Point(_x, _y) {
+	
+	// Return a new point
+	return {x: _x, y: _y};
 	
 }
